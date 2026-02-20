591eab8ca052dc4007b1ddeff4a95590450c3683
MIGUELNEHEMIASPOLEOD <MIGUELNEHEMIASPOLEOD@SCS217763>
2026-02-20 20:20:10 -0300

    modificação do home

--- setup/urls.py
+++ setup/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from turtle import home
+
 from django.contrib import admin
+from core.views import home
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home,name='home'),
 ]
